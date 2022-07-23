437ee7d5d8e98b88e2fbf213c591dc6533f293a2
BunnyCheetos <109825738+BunnyCheetos@users.noreply.github.com>
2022-07-23 20:13:21 +0100

    Update milkdaddy.lua

--- milkdaddy.lua
+++ milkdaddy.lua
@@ -1,253 +1,61 @@
-local MILKHUBLOL = Instance.new("ScreenGui")
-local TextLabel = Instance.new("TextLabel")
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Milkhub v1 | Bunny Cheetos", "LightTheme")
 
---Properties:
+-- Main 
+local Main = Window:NewTab("Main")
+local MainSection = Main:NewSection("Section Name")
 
-MILKHUBLOL.Name = "MILKHUBLOL"
-MILKHUBLOL.Parent = game.CoreGui
+MainSection:NewButton("Aimbot", "Locks on to people", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/BunnyCheetos/MilkHub/main/buzzinaimbot"))()
 
-TextLabel.Parent = MILKHUBLOL
-TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-TextLabel.BackgroundTransparency = 1.000
-TextLabel.Size = UDim2.new(0, 1920, 0, 50)
-TextLabel.Font = Enum.Font.SourceSans
-TextLabel.Text = "Subscribe to Bunny Cheetos on YT"
-TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
-TextLabel.TextScaled = true
-TextLabel.TextSize = 14.000
-TextLabel.TextStrokeTransparency = 0.000
-TextLabel.TextWrapped = true
-
--- Aimbot
-
-local dwCamera = workspace.CurrentCamera
-local dwRunService = game:GetService("RunService")
-local dwUIS = game:GetService("UserInputService")
-local dwEntities = game:GetService("Players")
-local dwLocalPlayer = dwEntities.LocalPlayer
-local dwMouse = dwLocalPlayer:GetMouse()
-
-local settings = {
-    Aimbot = true,
-    Aiming = false,
-    Aimbot_AimPart = "Head",
-    Aimbot_TeamCheck = true,
-    Aimbot_Draw_FOV = true,
-    Aimbot_FOV_Radius = 255,
-    Aimbot_FOV_Color = Color3.fromRGB(0,255,0)
-}
-
-local fovcircle = Drawing.new("Circle")
-fovcircle.Visible = settings.Aimbot_Draw_FOV
-fovcircle.Radius = settings.Aimbot_FOV_Radius
-fovcircle.Color = settings.Aimbot_FOV_Color
-fovcircle.Thickness = 1
-fovcircle.Filled = false
-fovcircle.Transparency = 1
-
-fovcircle.Position = Vector2.new(dwCamera.ViewportSize.X / 2, dwCamera.ViewportSize.Y / 2)
-
-dwUIS.InputBegan:Connect(function(i)
-    if i.UserInputType == Enum.UserInputType.MouseButton2 then
-        settings.Aiming = true
-    end
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "🍆💦",
+        Text = "cummed in ur mouth",
+        Duration = 15,
+        Icon = 'rbxassetid://9369734460'
+    })
 end)
 
-dwUIS.InputEnded:Connect(function(i)
-    if i.UserInputType == Enum.UserInputType.MouseButton2 then
-        settings.Aiming = false
-    end
+MainSection:NewButton("ESP", "Shows were people are", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/BunnyCheetos/MilkHub/main/jasiobgasesp.jar"))()
+
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "🍆💦",
+        Text = "cummed in ur mouth",
+        Duration = 15,
+        Icon = 'rbxassetid://9369734460'
+   }) 
 end)
 
-dwRunService.RenderStepped:Connect(function()
-    
-    local dist = math.huge
-    local closest_char = nil
+MainSection:NewButton("Macro", "Press shift to be gay", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/BunnyCheetos/MilkHub/main/milkyspeed"))()
 
-    if settings.Aiming then
-
-        for i,v in next, dwEntities:GetChildren() do 
-
-            if v ~= dwLocalPlayer and
-            v.Character and
-            v.Character:FindFirstChild("HumanoidRootPart") and
-            v.Character:FindFirstChild("Humanoid") and
-            v.Character:FindFirstChild("Humanoid").Health > 0 then
-
-                if settings.Aimbot_TeamCheck == true and
-                v.Team ~= dwLocalPlayer.Team or
-                settings.Aimbot_TeamCheck == false then
-
-                    local char = v.Character
-                    local char_part_pos, is_onscreen = dwCamera:WorldToViewportPoint(char[settings.Aimbot_AimPart].Position)
-
-                    if is_onscreen then
-
-                        local mag = (Vector2.new(dwMouse.X, dwMouse.Y) - Vector2.new(char_part_pos.X, char_part_pos.Y)).Magnitude
-
-                        if mag < dist and mag < settings.Aimbot_FOV_Radius then
-
-                            dist = mag
-                            closest_char = char
-
-                        end
-                    end
-                end
-            end
-        end
-
-        if closest_char ~= nil and
-        closest_char:FindFirstChild("HumanoidRootPart") and
-        closest_char:FindFirstChild("Humanoid") and
-        closest_char:FindFirstChild("Humanoid").Health > 0 then
-
-            dwCamera.CFrame = CFrame.new(dwCamera.CFrame.Position, closest_char[settings.Aimbot_AimPart].Position)
-        end
-    end
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "🍆💦",
+        Text = "cummed in ur mouth",
+        Duration = 15,
+        Icon = 'rbxassetid://9369734460'
+   }) 
 end)
 
+MainSection:NewButton("Inf Jump", "Cummy UwU", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/BunnyCheetos/MilkHub/main/0xJump"))()
 
-local lplr = game.Players.LocalPlayer
-local camera = game:GetService("Workspace").CurrentCamera
-local CurrentCamera = workspace.CurrentCamera
-local worldToViewportPoint = CurrentCamera.worldToViewportPoint
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "🍆💦",
+        Text = "cummed in ur mouth",
+        Duration = 15,
+        Icon = 'rbxassetid://9369734460'
+   }) 
+end)
 
-local HeadOff = Vector3.new(0, 0.5, 0)
-local LegOff = Vector3.new(0,3,0)
-
-for i,v in pairs(game.Players:GetChildren()) do
-    local BoxOutline = Drawing.new("Square")
-    BoxOutline.Visible = false
-    BoxOutline.Color = Color3.new(0,0,0)
-    BoxOutline.Thickness = 3
-    BoxOutline.Transparency = 1
-    BoxOutline.Filled = false
-
-    local Box = Drawing.new("Square")
-    Box.Visible = false
-    Box.Color = Color3.new(1,1,1)
-    Box.Thickness = 1
-    Box.Transparency = 1
-    Box.Filled = false
-
-    function boxesp()
-        game:GetService("RunService").RenderStepped:Connect(function()
-            if v.Character ~= nil and v.Character:FindFirstChild("Humanoid") ~= nil and v.Character:FindFirstChild("HumanoidRootPart") ~= nil and v ~= lplr and v.Character.Humanoid.Health > 0 then
-                local Vector, onScreen = camera:worldToViewportPoint(v.Character.HumanoidRootPart.Position)
-
-                local RootPart = v.Character.HumanoidRootPart
-                local Head = v.Character.Head
-                local RootPosition, RootVis = worldToViewportPoint(CurrentCamera, RootPart.Position)
-                local HeadPosition = worldToViewportPoint(CurrentCamera, Head.Position + HeadOff)
-                local LegPosition = worldToViewportPoint(CurrentCamera, RootPart.Position - LegOff)
-
-                if onScreen then
-                    BoxOutline.Size = Vector2.new(1000 / RootPosition.Z, HeadPosition.Y - LegPosition.Y)
-                    BoxOutline.Position = Vector2.new(RootPosition.X - BoxOutline.Size.X / 2, RootPosition.Y - BoxOutline.Size.Y / 2)
-                    BoxOutline.Visible = true
-
-                    Box.Size = Vector2.new(1000 / RootPosition.Z, HeadPosition.Y - LegPosition.Y)
-                    Box.Position = Vector2.new(RootPosition.X - Box.Size.X / 2, RootPosition.Y - Box.Size.Y / 2)
-                    Box.Visible = true
-
-                    if v.TeamColor == lplr.TeamColor then
-                        BoxOutline.Visible = false
-                        Box.Visible = false
-                    else
-                        BoxOutline.Visible = true
-                        Box.Visible = true
-                    end
-
-                else
-                    BoxOutline.Visible = false
-                    Box.Visible = false
-                end
-            else
-                BoxOutline.Visible = false
-                Box.Visible = false
-            end
-        end)
-    end
-    coroutine.wrap(boxesp)()
+MainSection:NewButton("Lag the living cum out of ur game", "Cummy UwU", function()
+while wait(0.1) do
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "🍆💦",
+        Text = "we like cuming in ur mouth",
+        Duration = 15,
+        Icon = 'rbxassetid://9369734460'
+   }) 
 end
-
-game.Players.PlayerAdded:Connect(function(v)
-    local BoxOutline = Drawing.new("Square")
-    BoxOutline.Visible = false
-    BoxOutline.Color = Color3.new(0,0,0)
-    BoxOutline.Thickness = 3
-    BoxOutline.Transparency = 1
-    BoxOutline.Filled = false
-
-    local Box = Drawing.new("Square")
-    Box.Visible = false
-    Box.Color = Color3.new(1,1,1)
-    Box.Thickness = 1
-    Box.Transparency = 1
-    Box.Filled = false
-
-    function boxesp()
-        game:GetService("RunService").RenderStepped:Connect(function()
-            if v.Character ~= nil and v.Character:FindFirstChild("Humanoid") ~= nil and v.Character:FindFirstChild("HumanoidRootPart") ~= nil and v ~= lplr and v.Character.Humanoid.Health > 0 then
-                local Vector, onScreen = camera:worldToViewportPoint(v.Character.HumanoidRootPart.Position)
-
-                local RootPart = v.Character.HumanoidRootPart
-                local Head = v.Character.Head
-                local RootPosition, RootVis = worldToViewportPoint(CurrentCamera, RootPart.Position)
-                local HeadPosition = worldToViewportPoint(CurrentCamera, Head.Position + HeadOff)
-                local LegPosition = worldToViewportPoint(CurrentCamera, RootPart.Position - LegOff)
-
-                if onScreen then
-                    BoxOutline.Size = Vector2.new(1000 / RootPosition.Z, HeadPosition.Y - LegPosition.Y)
-                    BoxOutline.Position = Vector2.new(RootPosition.X - BoxOutline.Size.X / 2, RootPosition.Y - BoxOutline.Size.Y / 2)
-                    BoxOutline.Visible = true
-
-                    Box.Size = Vector2.new(1000 / RootPosition.Z, HeadPosition.Y - LegPosition.Y)
-                    Box.Position = Vector2.new(RootPosition.X - Box.Size.X / 2, RootPosition.Y - Box.Size.Y / 2)
-                    Box.Visible = true
-
-                    if v.TeamColor == lplr.TeamColor then
-                        BoxOutline.Visible = false
-                        Box.Visible = false
-                    else
-                        BoxOutline.Visible = true
-                        Box.Visible = true
-                    end
-
-                else
-                    BoxOutline.Visible = false
-                    Box.Visible = false
-                end
-            else
-                BoxOutline.Visible = false
-                Box.Visible = false
-            end
-        end)
-    end
-    coroutine.wrap(boxesp)()
 end)
-
-local Player = game:GetService("Players").LocalPlayer
-local UIS = game:GetService("UserInputService")
-
-UIS.InputBegan:connect(function(UserInput)
-        if UserInput.UserInputType == Enum.UserInputType.Keyboard and UserInput.KeyCode == Enum.KeyCode.F then
-            _G.Running = true
-                while wait(0.005) and _G.Running == true do
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame + game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.lookVector * 1
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame + game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.lookVector * 1
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame + game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.lookVector * 1
-end
-        end
-end)
-UIS.InputEnded:connect(function(UserInput)
-        if UserInput.UserInputType == Enum.UserInputType.Keyboard and UserInput.KeyCode == Enum.KeyCode.F then
-                _G.Running = false
-        end
-end)
-
-while wait(6) do 
-    local A_1 = "Sub TO Bunny Cheetos on YT!" 
-    local A_2 = "All" 
-    local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest Event:FireServer(A_1, A_2)
-end
