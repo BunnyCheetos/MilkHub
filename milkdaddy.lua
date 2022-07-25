960a9c9774cbb7c168a9b7f3e5a40345038df702
BunnyCheetos <109825738+BunnyCheetos@users.noreply.github.com>
2022-07-25 18:34:05 +0100

    Added game hub

--- milkdaddy.lua
+++ milkdaddy.lua
@@ -3,7 +3,7 @@ local Window = Library.CreateLib("Milkhub v1 | Bunny Cheetos", "LightTheme")
 
 -- Main 
 local Main = Window:NewTab("Main")
-local MainSection = Main:NewSection("Section Name")
+local MainSection = Main:NewSection("Main Features")
 
 MainSection:NewButton("Aimbot", "Locks on to people", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/BunnyCheetos/MilkHub/main/buzzinaimbot"))()
@@ -56,6 +56,54 @@ while wait(0.1) do
         Text = "we like cuming in ur mouth",
         Duration = 15,
         Icon = 'rbxassetid://9369734460'
+   })
+end 
+end)
+
+--Game Scripts
+local MiscWin = Window:NewTab("Games")
+local MiscSection = MiscWin:NewSection("Select a script for your game")
+
+MiscSection:NewButton("Starving Artist", "Cummy UwU", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/BunnyCheetos/MilkHub/main/artist32lualibn789.cpp"))()
+
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "🍆💦",
+        Text = "cummed in ur mouth",
+        Duration = 15,
+        Icon = 'rbxassetid://9369734460'
    }) 
-end
+end)
+
+MiscSection:NewButton("Jailbreak", "Cummy UwU", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/wawsdasdacx/ohascriptnrrewading/main/jbsaxcriptidk1"))()
+
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "🍆💦",
+        Text = "cummed in ur mouth",
+        Duration = 15,
+        Icon = 'rbxassetid://9369734460'
+   }) 
+end)
+
+MiscSection:NewButton("Bedwars", "Cummy UwU", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua"))()
+
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "🍆💦",
+        Text = "cummed in ur mouth",
+        Duration = 15,
+        Icon = 'rbxassetid://9369734460'
+   }) 
+end)
+
+MiscSection:NewButton("Bloxburg (MIGHT GET YOU BANNED!)", "Cummy UwU", function()
+    loadstring(game:HttpGet('https://pastebin.com/raw/XB67JnAu'))()
+
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "🍆💦",
+        Text = "cummed in ur mouth",
+        Duration = 15,
+        Icon = 'rbxassetid://9369734460'
+   }) 
 end)
